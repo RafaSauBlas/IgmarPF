16ac79a7359a49956ef2068525f3cf4cbd402c28
RafaSauBlas <RafaSauBlas@hotmail.com>
2022-04-25 00:38:26 -0500

    cambios

--- vendor/swiftmailer/swiftmailer/lib/dependency_maps/transport_deps.php
+++ vendor/swiftmailer/swiftmailer/lib/dependency_maps/transport_deps.php
@@ -72,7 +72,7 @@ Swift_DependencyContainer::getInstance()
     ->asNewInstanceOf('Swift_Transport_Esmtp_Auth_CramMd5Authenticator')
 
     ->register('transport.loginauth')
-    ->asNewInstanceOf('Swift_Transport_Esmtp_Auth_loginAuthenticator')
+    ->asNewInstanceOf('Swift_Transport_Esmtp_Auth_LoginAuthenticator')
 
     ->register('transport.plainauth')
     ->asNewInstanceOf('Swift_Transport_Esmtp_Auth_PlainAuthenticator')
